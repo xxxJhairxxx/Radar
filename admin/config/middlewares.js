b8a5fe4ed8141ae5b84f013a7b059d13332c6592
xxxjhairxxx <jinfanzonquispe30@gmail.com>
2025-06-09 01:33:35 -0500

    first commit

---
+++ admin/config/middlewares.js
@@ -0,0 +1,50 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://market-assets.strapi.io',
+            'https://console.cloudinary.com',
+            'https://res.cloudinary.com',
+          ],
+          'script-src': [
+            "'self'",
+            'https://media-library.cloudinary.com',
+            'https://upload-widget.cloudinary.com',
+            'https://console.cloudinary.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://console.cloudinary.com',
+            'https://res.cloudinary.com',
+          ],
+          'frame-src': [
+            "'self'",
+            'https://media-library.cloudinary.com',
+            'https://upload-widget.cloudinary.com',
+            'https://console.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
